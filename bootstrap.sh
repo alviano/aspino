f7e227af4608c139559f313dee2c36d5f3e9d103
Mario Alviano <mario@alviano.net>
2014-12-09 18:41:34 +0100

    Add patch to glucose to handle reasons from propagators.

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,7 @@ echo "Patching glucose 4.0..."
 cd $DIRNAME/patches; rm -rf glucose-syrup; tar xf glucose-syrup.tgz; cd $OLDPATH
 cd $DIRNAME/patches/glucose-syrup; patch -p1 <../glucose-syrup.4.0.patch.1; cd $OLDPATH
 cd $DIRNAME/patches/glucose-syrup; patch -p1 <../glucose-syrup.4.0.patch.2; cd $OLDPATH
+cd $DIRNAME/patches/glucose-syrup; patch -p1 <../glucose-syrup.4.0.patch.3; cd $OLDPATH
 mv $DIRNAME/patches/glucose-syrup $DIRNAME/src/
 
 echo "Downloading gflags 2.1.1..."
